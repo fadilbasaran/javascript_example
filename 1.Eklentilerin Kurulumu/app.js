305973becced8d633ba804dfcb8b1499f22f6f5c
Fadıl Başaran <59345355+fadilbasaran@users.noreply.github.com>
2024-01-06 00:15:52 +0300

    1|Eklentilerin kurulumu

---
+++ 1.Eklentilerin Kurulumu/app.js
@@ -0,0 +1,12 @@
+//Beni Anla
+//*Beni Anla 
+//? Mavi renkte yazar
+//! Copyright 2014 Ben 
+//Todo: Yapılacaklar
+/*
+console.log("Fadıl");
+console.log("Fadıl");
+console.log("Fadıl");
+console.log("Fadıl");
+*/
+
