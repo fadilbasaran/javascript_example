3c309bf2685faec8cff09dc04e512a607052a16d
Fadıl Başaran <59345355+fadilbasaran@users.noreply.github.com>
2024-01-06 01:17:18 +0300

    3|Çıktı alma

---
+++ 3.Çıktı Verme/app.js
@@ -0,0 +1,35 @@
+//Documnet Write  ile çıktı alma
+
+// document.writeln("Hello world <br>")
+// document.writeln("Hello world")
+// document.writeln(4.3)
+
+
+//console.log ile çıktı alma
+
+// console.log("Hello world");
+// console.log("Fadıl Başaran");
+// console.log(5.4);
+// console.log(true);
+// console.log({ad:"Ali",yas:34});
+
+// let a=4;
+// let b=5;
+
+// console.log("Toplam değer: ",a+b);
+
+// console.clear();
+
+//Alert(Uyarı ile çıktı verme işlemleri)
+
+// console.log(window);
+
+// let a=3;
+// let b=6;
+// alert("a ile b nin toplamı : "+(a+b));
+
+// console.log("Fadıl Başaran");
+// debugger;
+// console.log("Fadıl Başaran");
+// console.log("Fadıl Başaran");
+// console.log("Fadıl Başaran");
