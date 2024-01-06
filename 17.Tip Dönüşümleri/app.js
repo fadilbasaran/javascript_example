c3c9817add5aca800872e7f7a9c5274d7388a273
Fadıl Başaran <59345355+fadilbasaran@users.noreply.github.com>
2024-01-06 15:58:17 +0300

    17|Tip dönüşümleri

---
+++ 17.Tip Dönüşümleri/app.js
@@ -0,0 +1,21 @@
+let i=34;
+let s= i.toString();
+let s2 =String(i);
+
+
+console.log(typeof s);
+console.log(typeof s2);
+
+let sT="3";
+let iN=Number(sT);
+let iN2=parseInt(sT);
+
+console.log(typeof iN);
+console.log(typeof iN2);
+
+let sF="3";
+
+let f=parseFloat(sF);
+console.log(typeof f);
+
+
