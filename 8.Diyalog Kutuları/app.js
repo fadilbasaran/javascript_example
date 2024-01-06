6535584bc19d820af653764066b0c3383a6a652d
Fadıl Başaran <59345355+fadilbasaran@users.noreply.github.com>
2024-01-06 13:54:13 +0300

    8|Diyalog Kutuları

---
+++ 8.Diyalog Kutuları/app.js
@@ -0,0 +1,18 @@
+// *-----DİYALOG KUTULARIT -----
+/*
+1--ALERT
+2-- Propt
+3--Confirm
+
+*/
+
+
+//alert("bilgi vermek için kullanılır.");
+
+let isim = prompt("İsminizi giriniz : ");
+let yas = prompt("İsminizi giriniz : ");
+
+alert(`İsminiz : ${isim} yas : ${yas}`);
+
+let yanit = confirm("Silmek istediğinize emin misiniz?")
+console.log(yanit?"Silindi!":"İptal edildi");
