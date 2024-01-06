c03cc9b0fe17c87236b48e059a66df8617f99f96
Fadıl Başaran <59345355+fadilbasaran@users.noreply.github.com>
2024-01-06 14:09:55 +0300

    9|Koşul yapıları

---
+++ 9.Koşul Yapıları/app.js
@@ -0,0 +1,14 @@
+
+let vize1= Number(prompt("Vize1 giriniz : "));
+let vize2= Number(prompt("Vize2 giriniz : "));
+let final= Number(prompt("Final giriniz : "));
+
+
+let ortalama= vize1*0.3+vize2*0.3+final*0.4;
+
+if (ortalama>65) {
+    alert("Tebrikler geçtiniz.");
+
+}else{
+    alert("Maalesef kaldınız.");
+}
