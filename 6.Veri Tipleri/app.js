ce752abefa2e113c32e477c4f14ae7bbb572a148
Fadıl Başaran <59345355+fadilbasaran@users.noreply.github.com>
2024-01-06 05:12:08 +0300

    6| Veri tipleri.

---
+++ 6.Veri Tipleri/app.js
@@ -0,0 +1,43 @@
+/*
+?Veri Tipleri
+
+1-String
+2-Number
+3-Boolean
+4-Null
+5-Undefined
+6-Object
+7-Function
+
+*/
+
+// var isim="Ali";
+// console.log(isim);
+// console.log(typeof isim);//String türü
+
+// let num=34.5;//virgüllü sayı veya tam sayı farketmeksizin hepsi number olarak geçer
+// console.log(num);
+// console.log(typeof num);
+
+// ?Boolean true veya false döner. null içi boş sonradan içine istediğimiz değişkendi koyabiliriz
+
+// let a;
+// console.log(a);// ?Undefined tanımlanmamış olab veri tipidir
+
+// let insan = {
+// ad:"Fadıl",
+// soyad:"Başaran",
+// yas:23
+// };
+// console.log(insan);
+// console.log(typeof obje);//object veri tipidir
+
+// let arr=[1,23,4,3];
+// console.log(typeof arr);//dizilerde obje türüdür.
+
+let func = function selamVer() {
+console.log("Selamm");
+};
+
+console.log(typeof func);//Tipi function olarak belirlenir
+console.log(f );
