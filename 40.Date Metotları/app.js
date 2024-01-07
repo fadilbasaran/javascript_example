a16032b5ea41407bf8cc015f8cf6584bb3844a6c
Fadıl Başaran <59345355+fadilbasaran@users.noreply.github.com>
2024-01-07 12:22:07 +0300

    40.Date Metotları

---
+++ 40.Date Metotları/app.js
@@ -0,0 +1,20 @@
+//* Date Methods
+
+let date = new Date(1999,1,17,0,12,23);//Spesifik bir tarihte yazılabilir
+console.log(date);
+// console.log(date);
+// console.log(date.getHours()); 
+// console.log(date.getDate());//Ayın kaçıncı günü alınıyor
+// console.log(date.getDay());//Get
+
+//SET Methods
+
+// console.log(date); 
+// date.setHours(15);//Saat 15.00 olacak
+// date.setDate(23);//Gün 23 olacak
+// date.setMonth(11 );//0 dan başladığı için aralık 0--11 arasında olmaktadır.
+// date.setMinutes(33);// Dakika değişmektedir.
+// console.log(date);
+
+ 
+
