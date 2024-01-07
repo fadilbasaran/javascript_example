d68335b833ad3f48a36c054b7636f6f32165b33b
Fadıl Başaran <59345355+fadilbasaran@users.noreply.github.com>
2024-01-07 11:32:57 +0300

    39|Math Metotları

---
+++ 39.Math Metotları/app.js
@@ -0,0 +1,33 @@
+//? Math Methods
+/**
+ 1-Floor
+ 2-Ceil
+ 3-Round
+
+ 4-Max
+ 5-Min
+
+ 6-Random
+ 7-Abs
+ 8-Sqrt
+ 9-Pow
+ --->PI Sayısı
+
+ */
+
+let a=3.99;
+// console.log(Math.floor(a));//Bir alta yuvarlar
+// console.log(Math.ceil(a));//Bir üste yuvarlar
+// console.log(Math.round(a));//En yakın sınıra yuvarlar
+// console.log(Math.max(1,3,4,5,45));//MAx değer geri döndürür
+// console.log(Math.min(1,3,4,5,45));//Min değer geri döndürür
+
+
+// console.log(Math.abs(-5));//Mutlak değer almak için kullanılır
+// console.log(Math.sqrt(49));//Karekökünü alır
+// console.log(Math.pow(3,3));//Üst almak için kullanılır
+
+// console.log(Math.PI);
+
+// console.log(Math.round((Math.random()*100000)));//0-1 arasında  bir değer döndürür
+ 
