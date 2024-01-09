10653d213ba55a1621ac35c8c1e9948bc67acd53
Fadıl Başaran <59345355+fadilbasaran@users.noreply.github.com>
2024-01-09 13:31:28 +0300

    58|Fetch API

---
+++ 58.Asenkron Programlama/6.Fetch API/app.js
@@ -0,0 +1,19 @@
+
+function getData(url) {
+    return fetch(url)
+    .then(response=>response.json())
+    .then(data=>console.log(data))
+    .catch(err=>console.log(err));
+}
+
+getData("students.json");
+getData("https://jsonplaceholder.typicode.com/posts/1");
+
+
+function saveStudents(params) {
+    fetch("https://jsonplaceholder.typicode.com/posts/1",{
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({})
+    });
+}
